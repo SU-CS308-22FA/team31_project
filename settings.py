7ab1ef99b6e168aad3d575f54badbd0de9b85013
berkantkartop <98823236+berkantkartop@users.noreply.github.com>
2023-01-05 12:06:00 +0300

    Heroku push

--- settings.py
+++ settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-v%1wvg29au0g95v886+6ktk2gzo(f_a8y_$g9*=#5my)1+3(_*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
